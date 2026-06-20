510f0431fc09fcdab6226fc35e35afbace0a1697
Viktor Tar <tarviky@gmail.com>
2026-06-20 03:34:24 +0300

    initial deploy

--- middleware.ts
+++ middleware.ts
@@ -10,15 +10,6 @@ export function middleware(req: NextRequest) {
   const accessToken = req.cookies.get("accessToken")?.value;
   const refreshToken = req.cookies.get("refreshToken")?.value;
 
-  console.log("========== MIDDLEWARE ==========");
-  console.log("PATH:", pathname);
-  console.log("ACCESS TOKEN EXISTS:", !!accessToken);
-  console.log("REFRESH TOKEN EXISTS:", !!refreshToken);
-  console.log(
-    "COOKIE HEADER:",
-    req.headers.get("cookie") ?? "NO COOKIE HEADER",
-  );
-
   const isPrivateRoute = privateRoutes.some((route) =>
     pathname.startsWith(route),
   );
@@ -29,26 +20,15 @@ export function middleware(req: NextRequest) {
 
   const isAuthenticated = Boolean(accessToken || refreshToken);
 
-  console.log("IS PRIVATE:", isPrivateRoute);
-  console.log("IS PUBLIC:", isPublicRoute);
-  console.log("IS AUTHENTICATED:", isAuthenticated);
-
   // not logged in → block private
   if (!isAuthenticated && isPrivateRoute) {
-    console.log("REDIRECTING TO SIGN-IN (NO TOKENS FOUND)");
-
     return NextResponse.redirect(new URL("/sign-in", req.url));
   }
 
   // logged in → block auth pages
   if (isAuthenticated && isPublicRoute) {
-    console.log("REDIRECTING TO PROFILE (ALREADY AUTHENTICATED)");
-
     return NextResponse.redirect(new URL("/profile", req.url));
   }
-
-  console.log("ALLOWING REQUEST");
-  console.log("==============================");
 
   return NextResponse.next();
 }
